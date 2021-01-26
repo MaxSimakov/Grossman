6e0a0254b4f826dfd80b0a1952821bd20eb6ed3b
MaxSimakov <Simakov.maxim82@gmail.com>
2021-01-27 00:11:02 +0300

    corrected map

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -12,32 +12,6 @@ function eventHandler() {
 		document.body.insertAdjacentHTML("beforeend", `<div class="pixel-perfect" style="background-image: url(screen/${screenName});"></div>`);
 	}
 
-	if ($("div").is(".contacts-page__map")) {
-		ymaps.ready(function () {
-			 var myMap = new ymaps.Map('map', {
-				  center: [55.72259807, 37.7027955],
-				  zoom: 9,
-				  controls: []
-			 }, {
-				  searchControlProvider: 'yandex#search'
-			 }),
-				  myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-				  }, {
-						iconLayout: 'default#image',
-						// Своё изображение иконки метки.
-						iconImageHref: '../assets/images/map-marker.png',
-						// Размеры метки.
-						iconImageSize: [37, 50],
-						// Смещение левого верхнего угла иконки относительно
-						// её "ножки" (точки привязки).
-						iconImageOffset: [-18.5, -25]
-				  });
-			 myMap.geoObjects
-				  .add(myPlacemark);
-		});
-  }
-
-
 };
 
 if (document.readyState !== 'loading') {
